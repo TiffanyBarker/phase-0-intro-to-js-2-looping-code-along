fe061d4289768aeb940baebc9363353cc0ef8115
Tiffany Barker <Tiffany.Barker13@yahoo.com>
2022-12-05 17:30:55 -0500

    commit tree 2b49faf80a9d7cdca81d23822a004f693b033f25

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Tiffany", "Darrin", "Shaggy"];
+const event = ["Birthday"]
+
+function writeCards(names,event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        messages.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+writeCards(names, event);
+
+
+function countDown(){
+    let number = 10;
+    while (number >=0){
+        console.log(number--);
+        debugger;
+    }
+    return number
+}
+countDown(10);
